e3c3ae95f202aba452004a82cac8d47eb152ac5c
LuciaIsiegas <24.damt.luciaisiegas@fundacionmontessori.com>
2025-02-12 16:27:55 +0100

    Creación de repositorio

---
+++ aplicacion_bbdd/src/aplicacion_bbdd/Principal.java
@@ -0,0 +1,17 @@
+package aplicacion_bbdd;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int opcion = 0;
+		do {
+			
+		} while(opcion != 10);
+
+	}
+
+}
