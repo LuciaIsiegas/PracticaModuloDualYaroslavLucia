af9f1c9f1404983fe3f57fd8dbd8ccdd19a4a94d
LuciaIsiegas <24.damt.luciaisiegas@fundacionmontessori.com>
2025-02-12 20:49:31 +0100

    Metodo submenu

--- aplicacion_bbdd/src/aplicacion_bbdd/Metodos.java
+++ aplicacion_bbdd/src/aplicacion_bbdd/Metodos.java
@@ -1,5 +1,21 @@
 package aplicacion_bbdd;
 
+import java.util.Scanner;
+
 public class Metodos {
+	
+	public static int seleccionTabla(String opcion, Scanner sc) {
+		System.out.print(opcion + ": \n"
+				+ "1. Estadisticas\n"
+				+ "2. Jugadores\n"
+				+ "3. Equipos\n"
+				+ "4. Partidos\n"
+				+ "Tabla seleccionada: ");
+		String aux = sc.nextLine();
+		
+		if (aux.matches("[1-4]")) {
+			return Integer.parseInt(aux);
+		}
+	}
 
 }
